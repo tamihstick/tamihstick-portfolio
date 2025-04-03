f4880b7d2141172f25366ba1a8b896aa53c9d064
ErjhonBaldoza <erjhonbaldoza@gmail.com>
2025-04-03 16:23:02 +0800

    portfolioIntro

---
+++ src/components/DiamondShape2.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DiamondShape2: React.FC = () => {
+  return (
+    <div className="w-full h-full border-8  shadow-silver border-white transform rotate-45"></div>
+  );
+};
+
+export default DiamondShape2;
---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div className="text-center text-7xl font-extrabold text-white flex absolute m-auto">
+      tamihstick
+    </div>
+  );
+};
+
+export default Logo;
